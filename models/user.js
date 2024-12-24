edccc7a234a6e3708c631c2d13db496d83eb4fa3
Wassim <wassim.besghayer@gmail.com>
2024-12-24 10:47:49 +0100

    update...

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const userSchema = new Schema({
-   name : String,
-   surname : String,
-   age : Number,
-   email : {type : String, unique : true},
-   isAdmin : {type : Boolean, default : false},
-   Image : String,
-  });
+const userSchema = new schema({
+  name:String,
+  surname:String,
+  age:Number,
+  email:{type:String, unique:true},
+  isAdmin:{type:Boolean, default:false},
+  Image:String,
+});
 
 
   const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports=User
