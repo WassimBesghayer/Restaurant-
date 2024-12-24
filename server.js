2da876d9e72e6998cb185f03860513b29f54b372
Wassim <wassim.besghayer@gmail.com>
2024-12-24 12:19:45 +0100

    node JS

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const db_connect = require("./conect_db");
+const db_connect = require("./connect_db");
+app.use(express.json());
 db_connect();
-
+// app.get("/wassim", (req, res)=>res.send("hello"))
 app.listen(process.env.PORT, (err) => err ? console.log(err) : console.log("server is running well"));
-app.use("/user", require("./routes/user"))
+app.use("/user", require("./routes/users"))
